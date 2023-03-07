8985c6c668e04dc998e9c9126278518fd2ce4f25
Ananasovna <a.n.ushkova@yandex.ru>
2023-03-07 21:33:49 +0200

    init: init project

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+
+
+module.exports = {
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx,html}",
+  ],
+  theme: {
+    colors: {
+      'black': '#000000',
+      'white': '#FFFFFF',
+    },
+    extend: { },
+  },
+  plugins: [],
+}
