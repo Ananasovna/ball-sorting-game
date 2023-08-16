c272dcf03036cd6937bd10573c85b8d9ab78e8b1
Ananasovna <a.n.ushkova@yandex.ru>
2023-08-16 22:17:39 +0200

    feat: add new ball style

--- src/components/Ball.tsx
+++ src/components/Ball.tsx
@@ -8,23 +8,24 @@ interface BallProps {
 }
 
 export const Ball: React.FC<BallProps> = ({children, ball}) => {
-  const classes = {
-    0: 'bg-0',
-    1: 'bg-1',
-    2: 'bg-2',
-    3: 'bg-3',
-    4: 'bg-4',
-    5: 'bg-5',
-    6: 'bg-6',
-    7: 'bg-7',
-    8: 'bg-8',
-    9: 'bg-9',
-    10: 'bg-10',
-    11: 'bg-11',
-  }
+  // const classes = {
+  //   0: 'bg-0',
+  //   1: 'bg-1',
+  //   2: 'bg-2',
+  //   3: 'bg-3',
+  //   4: 'bg-4',
+  //   5: 'bg-5',
+  //   6: 'bg-6',
+  //   7: 'bg-7',
+  //   8: 'bg-8',
+  //   9: 'bg-9',
+  //   10: 'bg-10',
+  //   11: 'bg-11',
+  // }
 
   return (
-    <div className={classNames(`${classes[ball]}`, 'h-8 w-8 rounded-full  border-black m-1 shrink-0 inner-shadow-ball')} data-number={ball}>
+    // <div className={classNames(`${classes[ball]}`, 'h-8 w-8 rounded-full  border-black m-1 shrink-0 inner-shadow-ball')} data-number={ball}>
+    <div className={classNames(`bg-${ball} h-8 w-8 rounded-full  border-black m-1 shrink-0 inner-shadow-ball`)} data-number={ball}>
       {children}
     </div>
   )
